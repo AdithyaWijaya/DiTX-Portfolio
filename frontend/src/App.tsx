9f0dfc1058521adda3708a68f0852fee63d844d5
Adithya <adithyawijayajr@gmail.com>
2026-08-25 12:57:42 +0800

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+export default function OldPort () {
+  return (
+    <iframe
+      src="/portLogacy/index.html"
+      title="Old Portfolio"
+      style={{
+        width: "100%",
+        height: "100vh",
+        border: "none",
+      }}
+    />
+  );
+}
