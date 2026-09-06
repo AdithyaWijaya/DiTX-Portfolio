dcf0230ab3f5d5fffa10d1624735202e138b6918
Adithya <adithyawijayajr@gmail.com>
2026-09-06 09:53:24 +0800

    Add Test component and initial index.html file

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+export default function Test () {
+  return (
+    <iframe
+      src="/index.html"
+      title="TEST"
+      style={{
+        width: "100%",
+        height: "100vh",
+        border: "none",
+      }}
+    />
+  );
+}
